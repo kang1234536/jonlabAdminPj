87ac55acdb987a1c43394cade430b10ef234adfa
강희원 <kang1234536@github.com>
2024-04-14 19:37:42 +0900

    test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      // target: ' https://commonserver-1be945854c6e.herokuapp.com',
-      target: ' http://localhost:6060',
+      target: ' https://commonserver-1be945854c6e.herokuapp.com', 
+      // target: ' http://localhost:6060',
       changeOrigin: true,
     })
   );
